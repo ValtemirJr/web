86707859d57e639696ade5f55ff3d12fc3160966
ValtemirJr <valtemir.junior53@gmail.com>
2023-02-22 16:56:05 -0300

    first commit

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+         public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                {"/Livros/ParaLer", _repo.ParaLer.ToString() },
+                {"/Livros/Lendo", _repo.Lendo.ToString() },
+                {"/Livros/Lidos", _repo.Lidos.ToString() }
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+            return context.Response.WriteAsync("Caminho Inexistente!!!");
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+    }
+}
